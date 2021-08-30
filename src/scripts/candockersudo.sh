2c4f3705a2447d6431c3d5c892f5b8d5d240906b
Daniel <gut.daniel@pm.me>
2021-08-30 15:50:44 +0200

    adding sudo check; renaming steps

---
+++ src/scripts/candockersudo.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env sh
+
+TARGET_IMAGE=$(isopod image)
+
+is_sudo_available() {
+
+sudo_path=$(docker run "$TARGET_IMAGE" command -v sudo)
+
+if [ -z $sudo_path ];
+then
+    echo "sudo could not be found"
+    exit 0
+fi
+
+}
+
+get_sudo_permissions() {
+
+# Check for passwordless sudo permissions
+sudo_permssions=$(docker run "$TARGET_IMAGE" sudo -nl)
+
+if $(echo $sudo_permssions | grep -q not\ allowed);
+then
+  echo "User doesn't have passwordless sudo permissions"
+  return 0
+else
+  echo "User has passwordless sudo permissions"
+  echo $sudo_permssions
+  return 1
+fi
+
+}
+
+is_sudo_available
+get_sudo_permissions
+
