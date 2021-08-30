103cad0231f0fab1eaed28440a91f9457193bb13
Daniel <gut.daniel@pm.me>
2021-08-30 15:54:43 +0200

    fixing orb checker results

--- src/scripts/candockersudo.sh
+++ src/scripts/candockersudo.sh
@@ -19,13 +19,13 @@ get_sudo_permissions() {
 # Check for passwordless sudo permissions
 sudo_permssions=$(docker run "$TARGET_IMAGE" sudo -nl)
 
-if $(echo $sudo_permssions | grep -q not\ allowed);
+if $(echo "$sudo_permssions" | grep -q not\ allowed);
 then
   echo "User doesn't have passwordless sudo permissions"
   return 0
 else
   echo "User has passwordless sudo permissions"
-  echo $sudo_permssions
+  echo "$sudo_permssions"
   return 1
 fi
 
