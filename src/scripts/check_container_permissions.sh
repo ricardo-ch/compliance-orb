4c3d387ee41b717485fa55b9e840c85a2cc4d4ab
Daniel <gut.daniel@pm.me>
2021-09-02 15:23:00 +0200

    consolidate container permissions checks

--- src/scripts/check_container_permissions.sh
+++ src/scripts/check_container_permissions.sh
@@ -2,6 +2,18 @@
 
 TARGET_IMAGE=$(isopod image)
 
+is_docker_user_root() {
+uid=$(docker run "$TARGET_IMAGE" id -u)
+
+if [ "$uid" == 0 ];
+then
+  echo "Container is using Root user"
+  return 1
+fi
+
+return 0
+}
+
 is_sudo_available() {
 
 sudo_path=$(docker run "$TARGET_IMAGE" which sudo)
@@ -36,5 +48,8 @@ fi
 
 }
 
+
+
+is_docker_user_root
 get_sudo_permissions
 
