3e5e22af8cc732e94134e2b4cbfa83e6928ae6b3
Daniel <gut.daniel@pm.me>
2021-09-10 15:31:05 +0200

    shell check

--- src/scripts/check_container_user_permissions.sh
+++ src/scripts/check_container_user_permissions.sh
@@ -1,24 +1,18 @@
 #!/usr/bin/env sh
 
-
-
-if [ -z "${imageName}" ];
+if [ -z "${IMAGE_NAME}" ];
 then
   TARGET_IMAGE=$(isopod image)
 else
-  TARGET_IMAGE="${imageName}"
+  TARGET_IMAGE="${IMAGE_NAME}"
 fi
 
-
-
 is_docker_user_root() {
-
-
 uid=$(docker run "$TARGET_IMAGE" id -u)
 
 if [ "$uid" == 0 ];
 then
-  echo "Container is using Root user"
+  echo "Non compliance detected: Container is using Root user"
   return 1
 fi
 
@@ -26,21 +20,19 @@ return 0
 }
 
 is_sudo_available() {
-
 sudo_path=$(docker run "$TARGET_IMAGE" which sudo)
 
 if [ -z "$sudo_path" ];
 then
     return 1
 fi
-
 }
 
 get_sudo_permissions() {
 
 if ! is_sudo_available;
   then
-  echo "sudo could not be found"
+  echo "Info: sudo binary could not be found in container"
   return 0
 fi
 
@@ -49,17 +41,13 @@ sudo_permssions=$(docker run "$TARGET_IMAGE" sudo -nl)
 
 if echo "$sudo_permssions" | grep -q not\ allowed;
 then
-  echo "User doesn't have passwordless sudo permissions"
   return 0
 else
-  echo "User has passwordless sudo permissions"
+  echo "Non compliance detected: User has passwordless sudo permissions"
   echo "$sudo_permssions"
   return 1
 fi
-
 }
-
-
 
 is_docker_user_root
 get_sudo_permissions
