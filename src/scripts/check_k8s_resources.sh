449ef0c371a051e43a11db90571194d6c2759d9d
Daniel <gut.daniel@pm.me>
2021-09-17 16:17:35 +0200

    fixed bugs, ready for PR

--- src/scripts/check_k8s_resources.sh
+++ src/scripts/check_k8s_resources.sh
@@ -4,22 +4,19 @@ COMPLIANT=true
 
 render_k8s_resources() {
 
-  echo "Info: starting rendering"
-
   if [ ! -f "$ISOPOD_FILE" ]; then
       echo "$ISOPOD_FILE not found. Specify file path with 'isopodFile' in CirclCI. More infos in compliance-orb Readme"
       exit 1
   fi
 
   # ignore error of missing context. Command only needs to render new resources and doesn't care about already deployed
-  isopod -f "$ISOPOD_FILE" deploy -e prod --dry-run || true
-
-  echo "Info: rendering finished"
+  isopod -f "$ISOPOD_FILE" deploy -e prod --dry-run 2> /dev/null || true
+  echo "Info: Created:"
+  ls  /tmp/*-k8s-manifest-files*/*.yml
 }
 
 cleanup_and_quit() {
 
-  echo "Info: Cleaning up"
   rm output.json
   rm -rf /tmp/-k8s-manifest-files*
 
@@ -28,7 +25,6 @@ cleanup_and_quit() {
     exit 1
   fi
 
-  echo "Info: Done"
   exit 0
 }
 
@@ -55,28 +51,20 @@ EOT
 }
 
 check_privileged_flag() {
-echo "Info: Validating K8S resources"
+
 for file in /tmp/*-k8s-manifest-files*/*.yml
 do
-  echo "Info: Validating $file"
 
-  echo "Debug: Exit code $?"
-  opa eval -i "$file" -d container-privileged-flag.rego "data.kubernetes.validating.privileged"
-  echo "Debug: Exit code $?"
   opa eval -i "$file" -d container-privileged-flag.rego "data.kubernetes.validating.privileged" > output.json;
-  echo "Debug: Exit code $?"
-
   RESULT=$(jq .result[]?.expressions[]?.value.deny[]? < output.json)
-  echo "Debug: Exit code $?"
 
   if [ -n "$RESULT" ];
   then
     echo "Non compliance detected: $RESULT"
     COMPLIANT=false
   fi
-  echo "Debug: Exit code $?"
+
 done
-echo "Info: Finished validating K8S resources"
 
 }
 
