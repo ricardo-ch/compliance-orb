f0cac1284a1910254e621c7b5eb489e5e81d9fd3
Daniel <gut.daniel@pm.me>
2021-12-10 09:28:08 +0100

    using env var for endpoint

--- src/scripts/orb-funcs/submit-compliance-results.sh
+++ src/scripts/orb-funcs/submit-compliance-results.sh
@@ -26,7 +26,7 @@ append_checks () {
 }
 
 send_data () {
-    curl -X POST -H "Content-Type: application/json" -d @compliance-checks.json http://localhost:5000
+    curl -X POST -H "Content-Type: application/json" -d @compliance-checks.json ${SRE_API_COMPLIANCE_ENDPOINT}
 }
 
 create_umbrella_json
