751d763795402312b16ba7783a9aca035b2b3ad7
Louis Pouliquen <louis@lpo-debian.ricardo.ch>
2022-06-08 10:49:54 +0200

    modified:   src/scripts/orb-funcs/submit_compliance_report.sh

--- src/scripts/orb-funcs/submit_compliance_report.sh
+++ src/scripts/orb-funcs/submit_compliance_report.sh
@@ -10,7 +10,7 @@ create_json_compliance_query() {
         --arg commit_sha "$CIRCLE_SHA1" \
         --arg cc_url "$CIRCLE_BUILD_URL" \
         '{"application_name": $app, "branch": $branch, "commit_sha": $commit_sha , "ci_url": $cc_url, "checks": []}' \
-        >compliance_checks.json
+        >"$app_name_compliance_checks.json"
 }
 
 append_application_checks() {
@@ -20,23 +20,23 @@ append_application_checks() {
         [[ -e $file ]] || break
         tmpfile=$(mktemp)
 
-        jq ".checks[.checks| length] |= . +$(jq . "$file")" $app_name_compliance_checks.json >"${tmpfile}"
-        cat "${tmpfile}" > $app_name_compliance_checks.json
+        jq ".checks[.checks| length] |= . +$(jq . "$file")" "$app_name_compliance_checks.json" >"${tmpfile}"
+        cat "${tmpfile}" >"$app_name_compliance_checks.json"
 
     done
 }
 
 post_compliance_report() {
-    curl -X POST -H "Content-Type: application/json" -d @$app_name_compliance-checks.json "${SRE_API_COMPLIANCE_ENDPOINT}"
-}
+    app_name=$1
 
+    curl -X POST -H "Content-Type: application/json" -d @"$app_name_compliance-checks.json" "${SRE_API_COMPLIANCE_ENDPOINT}"
+}
 
 isopod_files=$(find . -regextype sed -regex ".*isopod.*\.yml")
 
 for file in $isopod_files; do
-  app_name=$(yq .metadata.labels.app $file)
-  create_compliance_query $app_name
-  append_application_checks $app_name
-  post_compliance_report $app_name
+    app_name=$(yq .metadata.labels.app $file)
+    create_compliance_query "$app_name"
+    append_application_checks "$app_name"
+    post_compliance_report "$app_name"
 done
-
