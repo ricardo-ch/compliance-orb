717cdfe8f8a773d8bb5387ff7eb8e8a58c1a6066
Louis Pouliquen <louis@lpo-debian.ricardo.ch>
2022-06-08 14:01:29 +0200

    modified:   src/scripts/orb-funcs/pull_application_images.sh

--- src/scripts/orb-funcs/pull_application_images.sh
+++ src/scripts/orb-funcs/pull_application_images.sh
@@ -4,6 +4,6 @@ set -o nounset
 isopod_files=$(find . -regextype sed -regex ".*isopod.*\.yml")
 
 for file in $isopod_files; do
-   image=$(isopod image -f $file)
+   image=$(isopod image -f "$file")
    docker pull "$image"
 done
