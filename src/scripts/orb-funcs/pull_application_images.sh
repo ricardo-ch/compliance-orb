36b53fe784842c9338c6dba02803d78693bfd911
Louis Pouliquen <louis@lpo-debian.ricardo.ch>
2022-06-21 12:07:19 +0200

    As announced compliance orb should never fail

--- src/scripts/orb-funcs/pull_application_images.sh
+++ src/scripts/orb-funcs/pull_application_images.sh
@@ -4,6 +4,6 @@ set -o nounset
 isopod_files=$(find . -regextype sed -regex ".*isopod.*\.yml")
 
 for file in $isopod_files; do
-   image=$(isopod image -f "$file")
-   docker pull "$image"
+   image=$(isopod image -f "$file" || true)
+   docker pull "$image" || true
 done
